5104222756e2948441f62fdddfcd08136324103e
fvasilets <fvasilets@modem.by>
2024-09-24 13:00:32 +0300

    рефакторинг

--- PipeDiagnosticsViewer/UI/Extensions/SelectedFirstRowBehavior.cs
+++ PipeDiagnosticsViewer/UI/Extensions/SelectedFirstRowBehavior.cs
@@ -14,18 +14,19 @@ namespace PipeDiagnosticsViewer.UI.Extensions
         protected override void OnAttached()
         {
             AssociatedObject.LoadingRow += LoadingRow;
-            DependencyPropertyDescriptor? dependencyPropertyDescriptor = DependencyPropertyDescriptor.FromProperty(ItemsControl.ItemsSourceProperty, AssociatedObject.GetType());
-            dependencyPropertyDescriptor.AddValueChanged(AssociatedObject, ItemsSourceChanged);
+            DependencyPropertyDescriptor? dependencyPropertyDescriptor = DependencyPropertyDescriptor.FromProperty(ItemsControl.ItemsSourceProperty, AssociatedType);
+            dependencyPropertyDescriptor?.AddValueChanged(AssociatedObject, ItemsSourceChanged);
         }
 
         protected override void OnDetaching()
         {
             AssociatedObject.LoadingRow -= LoadingRow;
             DependencyPropertyDescriptor? dependencyPropertyDescriptor = DependencyPropertyDescriptor.FromProperty(ItemsControl.ItemsSourceProperty, AssociatedType);
-            dependencyPropertyDescriptor.RemoveValueChanged(AssociatedObject, ItemsSourceChanged);
+            dependencyPropertyDescriptor?.RemoveValueChanged(AssociatedObject, ItemsSourceChanged);
             if (notifyCollectionChanged != null)
                 notifyCollectionChanged.CollectionChanged -= ItemsSourceCollectionChanged;
         }
+
 
         private void ItemsSourceChanged(object? sender, EventArgs e)
         {
@@ -36,23 +37,16 @@ namespace PipeDiagnosticsViewer.UI.Extensions
 
         private void ItemsSourceCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
         {
-            if (firstItem != null)
-                return;
-
-            if (e.NewStartingIndex == 0 && e.NewItems?.Count > 0)
+            if (firstItem == null && e.NewStartingIndex == 0 && e.NewItems?.Count > 0)
                 firstItem = e.NewItems[0];
         }
 
         private void LoadingRow(object? sender, DataGridRowEventArgs e)
         {
-            if (firstItem == null)
+            if (firstItem == null || !e.Row.Item.Equals(firstItem))
                 return;
 
-            DataGridRow row = e.Row;
-            if (!row.Item.Equals(firstItem))
-                return;
-
-            row.IsSelected = true;
+            e.Row.IsSelected = true;
             AssociatedObject.SelectedItem = firstItem;
             firstItem = null;
         }
